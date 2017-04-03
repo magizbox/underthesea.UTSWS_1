b3b2af6676e26ecf9e3200d21b1aed607a757b1b
anhv <=>
2017-04-03 18:11:18 +0700

    refactor code

--- maintaince/test_character_segmenter.py
+++ maintaince/test_character_segmenter.py
@@ -1,89 +1,95 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase
 
-from maintaince.character_segmenter import character_segmenter
+from maintaince.tokenize_sentence import tokenize_sentence
 
 
-class TestCharacter_segmenter(TestCase):
+class Test_TokenizeSentence(TestCase):
 
-    def test_character_segmenter(self):
-        input = ("300.000.000.000")
-        actual = character_segmenter(input)
-        expected = ("300.000.000.000")
+    def test_tokenize_sentence(self):
+        input = "300.000.000.000"
+        actual = tokenize_sentence(input)
+        expected = "300.000.000.000"
         self.assertEqual(actual, expected)
 
-    def test_character_segmenter_2(self):
+    def test_tokenize_sentence_2(self):
         input = u"Vụ “hot girl xứ Thanh” has 400.000 (USD) "
-        actual = character_segmenter(input)
+        actual = tokenize_sentence(input)
         expected = u'Vụ “ hot girl xứ Thanh ” has 400.000 ( USD ) '
         self.assertEqual(actual, expected)
 
-    def test_character_segmenter_3(self):
+    def test_tokenize_sentence_3(self):
         input = ("abc@gmail.com")
-        actual = character_segmenter(input)
+        actual = tokenize_sentence(input)
         expected = ("abc@gmail.com")
         self.assertEqual(actual, expected)
 
-    def test_character_segmenter_4(self):
+    def test_tokenize_sentence_4(self):
         input = u"giá chỉ từ 800.000đ - 3.000.000đ/1 vòng vàng charm. Một số xu hướng"
-        actual = character_segmenter(input)
+        actual = tokenize_sentence(input)
         expected = u"giá chỉ từ 800.000 đ - 3.000.000 đ / 1 vòng vàng charm . Một số xu hướng"
         self.assertEqual(actual, expected)
 
-    def test_character_segmenter_41(self):
+    def test_tokenize_sentence_42(self):
+        input = u"800.000VNĐ"
+        actual = tokenize_sentence(input)
+        expected = u"800.000 VNĐ"
+        self.assertEqual(actual, expected)
+
+    def test_tokenize_sentence_41(self):
         input = u"chỉ số 800.000VNĐ"
-        actual = character_segmenter(input)
+        actual = tokenize_sentence(input)
         expected = u"chỉ số 800.000 VNĐ"
         self.assertEqual(actual, expected)
 
-    def test_character_segmenter_5(self):
+    def test_tokenize_sentence_5(self):
         input = u"tăng tốc từ 0-100 km/h trong 3,2 giây và tốc độ tối đa 330 km/h. Tại Việt Nam "
-        actual = character_segmenter(input)
+        actual = tokenize_sentence(input)
         expected = u"tăng tốc từ 0 - 100 km / h trong 3,2 giây và tốc độ tối đa 330 km / h . Tại Việt Nam "
         self.assertEqual(actual, expected)
 
-    def test_character_segmenter_6(self):
+    def test_tokenize_sentence_6(self):
         input = u"ngày 6/2/2017: Kết quả xổ số điện toán 123 là 2 26 733; "
-        actual = character_segmenter(input)
+        actual = tokenize_sentence(input)
         expected = u"ngày 6 / 2 / 2017 : Kết quả xổ số điện toán 123 là 2 26 733 ; "
         self.assertEqual(actual, expected)
 
 
-    def test_character_segmenter_7(self):
+    def test_tokenize_sentence_7(self):
         input = u"https://github.com/JackNhat/underthesea.UTSWS_1"
-        actual = character_segmenter(input)
+        actual = tokenize_sentence(input)
         expected = u"https://github.com/JackNhat/underthesea.UTSWS_1"
         self.assertEqual(actual, expected)
 
 
 
-    def test_character_segmenter_8(self):
+    def test_tokenize_sentence_8(self):
         input = u"cơ. “Tôi đã bớt ngây thơ hơn trong cách nhìn nhận về thế giới, nhưng điều này vẫn cần học hỏi thêm”, anh nói. Minh Hải"
-        actual = character_segmenter(input)
+        actual = tokenize_sentence(input)
         expected = u"cơ . “ Tôi đã bớt ngây thơ hơn trong cách nhìn nhận về thế giới , nhưng điều này vẫn cần học hỏi thêm ” , anh nói . Minh Hải"
         self.assertEqual(actual, expected)
 
-    def test_character_segmenter_9(self):
+    def test_tokenize_sentence_9(self):
         input = u"Sen Vàng (P.Minh Khai, Q.Hai Bà Trưng, Hà Nội), "
-        actual = character_segmenter(input)
+        actual = tokenize_sentence(input)
         expected = u'Sen Vàng ( P . Minh Khai , Q . Hai Bà Trưng , Hà Nội ) , '
         self.assertEqual(actual, expected)
 
-    def test_character_segmenter_10(self):
+    def test_tokenize_sentence_10(self):
         input = u"->"
-        actual = character_segmenter(input)
+        actual = tokenize_sentence(input)
         expected = u"->"
         self.assertEqual(actual, expected)
 
-    def test_character_segmenter_11(self):
+    def test_tokenize_sentence_11(self):
         input = u"3,4"
-        actual = character_segmenter(input)
+        actual = tokenize_sentence(input)
         expected = u"3,4"
         self.assertEqual(actual, expected)
 
-    def test_character_segmenter_12(self):
+    def test_tokenize_sentence_12(self):
         input = u"google.com"
-        actual = character_segmenter(input)
+        actual = tokenize_sentence(input)
         expected = u"google.com"
         self.assertEqual(actual, expected)
 
--- maintaince/tokenize_sentence.py
+++ maintaince/tokenize_sentence.py
@@ -8,28 +8,31 @@ punctuations = open(join(dirname(__file__), "punctuation.txt"), "r").read().spli
 punctuations = [p.decode("utf-8") for p in punctuations]
 
 
-def token_segmenter(token):
-    digit_regex = "^\d+((\.\d+)|(\,\d))+$"
-    # money_regex = "^\d+(\.\d+)+\w$"
+def tokenize(token):
+    digit_regex = r"\b\d+([\.,]\d+)+\b"
     email_regex = "[^@]+@[^@]+\.[^@]+"
     web_regex = "^http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+$"
     web_2_regex = "\w+\.(com)"
     special_character = "->"
     specials = [digit_regex, email_regex, special_character, web_regex, web_2_regex]
     for special in specials:
-        if re.match(special, token):
+        if re.match(special, token, re.UNICODE):
             return token
+    # digit_regex_ = "\d+([\.,]\d+)+"
+    for pattern in specials:
+        if re.findall(pattern, token, re.UNICODE):
+            token = re.sub(pattern, lambda matched: matched.group() + " ", token)
+            return tokenize_sentence(token)
     for punctuation in punctuations:
         token = token.replace(punctuation, u" {} ".format(punctuation))
     return token
 
 
-def character_segmenter(sentence):
-    tokens = re.split(" ", sentence)
-    tokens = [token_segmenter(token) for token in tokens]
+def tokenize_sentence(sentence):
+    tokens = re.split("\s", sentence)
+    tokens = [tokenize(token) for token in tokens]
     output = u" ".join(tokens)
-    while "  " in output:
-        output = output.replace("  ", " ")
+    output = re.sub(r"\s+", " ", output)
     return output
 
 
@@ -39,5 +42,5 @@ if __name__ == '__main__':
     ids = listdir(file_path)
     ids = ids[:10]
     sentences = [open(join(out_file_path, id), "w").write(
-        character_segmenter(open(join(file_path, id), "r").read().decode('utf-8')).encode('utf-8')) for id in ids]
+        tokenize_sentence(open(join(file_path, id), "r").read().decode('utf-8')).encode('utf-8')) for id in ids]
     print 0
