e3c92af5e2cf77b181f5669cbdf8be1933bee974
Doan Viet Dung <doanvietdung273@gmail.com>
2017-04-13 15:56:24 +0700

    refactor code

--- maintaince/compare_dict.py
+++ maintaince/compare_dict.py
@@ -1,26 +1,18 @@
 # -*- coding: utf-8 -*-
 from os.path import join, dirname
-
+import underthesea
 from os import listdir
-from pandas import DataFrame, ExcelWriter
-import sys
+import pandas as pd
 from collections import Counter
-
-reload(sys)
-sys.setdefaultencoding('utf 8')
+#
+# reload(sys)
+# sys.setdefaultencoding('utf 8')
 
 model = "vntokenizer_1"
-# path_Dongdu = join(dirname(dirname(__file__)), "corpus", "output_Dongdu_1")
 path = join(dirname(dirname(__file__)), "corpus", "output_" + model)
-# path_jvntextpro = join(dirname(dirname(__file__)), "corpus", "output_jvntextpro_1")
-# path_pyvi = join(dirname(dirname(__file__)), "corpus", "output_pyvi_1")
-# path_vn_tokenize = join(dirname(dirname(__file__)), "corpus", "output_vntokenizer_1")
 ids = listdir(path)
-# f_word_in_dict = open(join(dirname(__file__), "word_analysis", model, "word_in_dict.txt"), "w")
-# f_word_not_in_dict = open(join(dirname(__file__), "word_analysis", model, "word_not_in_dict.txt"), "w")
-# f_result = open(join(dirname(__file__), "word_analysis", model, "result"), "w")
 dictionary = open('Viet74K.txt', "r").read().split('\n')
-dictionary = [word.replace(' ', '_') for word in dictionary if ' ' in word]
+words_74K = underthesea.corpus.viet_dict_74K.words
 
 
 def to_list(sentences):
@@ -40,41 +32,27 @@ def to_words(sentence):
     return [word for word in sentence.split(' ')]
 
 
-def compare_dict(filepath):
-    word_in_dict = []
-    word_not_in_dict = []
+def get_words(filepath):
+    words_total = []
     for id in ids:
         sentences = to_list(open(join(filepath, id), "r").read())
         words = [to_words(sentence) for sentence in sentences]
-        words = [word for sub_words in words for word in sub_words]
-        # total_words.append(words)
-        # total_words = [word for sub_words in total_words for word in sub_words]
-        for word in words:
-            if word in dictionary:
-                word_in_dict.append(word)
-            else:
-                word_not_in_dict.append(word)
-    return word_in_dict, word_not_in_dict
+        words = [word.replace('_', ' ') for sub_words in words for word in sub_words]
+        words_total.append(words)
+    words_total = [word.lower() for sub_words in words_total for word in sub_words]
+    words_total = [word.decode("utf-8") for word in words_total]
+    return words_total
 
 
-# word_in_dict_DongDu, word_not_in_dict_DongDu = compare_dict(path_Dongdu)
-writer_1 = ExcelWriter(join(dirname(__file__), "word_analysis", model, "word_in_dict.xlsx"))
-writer_2 = ExcelWriter(join(dirname(__file__), "word_analysis", model, "word_not_in_dict.xlsx"))
-# df = DataFrame(word_in_dict_DongDu)
-# df.index = df.index.str.encode('utf-8')
+words = get_words(path)
+words_count = pd.DataFrame.from_dict(Counter(words), orient='index')
+in_dict = list(set(words).intersection(set(words_74K)))
+not_in_dict = list(set(words).difference(set(words_74K)))
 
-word_in_dict, word_not_in_dict = compare_dict(path)
-word_in_dict = Counter(word_in_dict)
-df1 = DataFrame.from_dict(word_in_dict, orient='index').reset_index()
-df1.to_excel(writer_1)
-word_not_in_dict = Counter(word_not_in_dict)
-df2 = DataFrame.from_dict(word_not_in_dict, orient='index').reset_index()
-df2.to_excel(writer_2)
-writer_1.save()
-writer_2.save()
-# word_in_dict_jvn, word_not_in_dict_jvn = compare_dict(path_jvntextpro)
-# word_in_dict_pyvi, word_not_in_dict_pyvi = compare_dict(path_pyvi)
-# word_not_in_dict_vntokenizer, word_not_in_dict_vntokenizer = compare_dict(path_vn_tokenize)
-# words = [word_in_dict_DongDu, word_in_dict_UET, word_in_dict_jvn, word_in_dict_pyvi, word_not_in_dict_vntokenizer]
+in_dict = words_count.ix[in_dict, :].reset_index()
+in_dict.columns = ["word", "count"]
+out_dict = words_count.ix[not_in_dict, :].reset_index()
+out_dict.columns = ["word", "count"]
 
-print 0
+# in_dict.to_excel("in.xlsx", index=False)
+# out_dict.to_excel("out.xlsx", index=False)
